72e889324475ab063d977bf3a3d077f01e660086
Claudio-Pri <priv.claudio@gmail.com>
2024-08-30 17:50:18 +0200

    generati list item

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,41 @@
 console.log('js collegato');
-
 //===== Appunti ======
 // ` backtick
-
+// {{TEXT INTERPOLATION}}
+// v-bind:attributo="..js.."
+// :attributo="..js.."
+// v-on:click="..js.."
+// @click=""
 //===== Code ======
 
+// deconstructuring
+const { createApp } = Vue
 
-//===== Functions =====
+const app = createApp({
+    data() {
+        return {
+            todos: [
+                {
+                    text: 'Fare la spesa',
+                    done: false,
+                },
+                {
+                    text: 'Cucinare',
+                    done: false,
+                },
+                {
+                    text: 'Lavare i piatti',
+                    done: false,
+                },
+            ]
+        };
+    },
+    methods: {
+        removeToDo() {
+            console.log('rimuovi todo');
+        }
+    }
+
+
+
+}).mount('#app')
